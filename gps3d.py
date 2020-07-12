158ab1fe50e4132ac6225c298108dba543909993
Nicholas Reseburg <github@whiteseptember.org>
2020-07-11 18:56:49 -0700

    cleaned up testing code lines

--- gps3d.py
+++ gps3d.py
@@ -22,7 +22,8 @@ class myTrackPoint:
         self.elevation = ele
         self.gpstime = utcd
         self.localtime = locald
-        self.temperature = temp 
+        self.temperature = temp
+        self.model = 'Garmin GPSMAP 64sx' 
 
 
 
@@ -49,9 +50,6 @@ def generateCommaindxes(record):
         counter += 1
 
 def parseGPXLine(record):
-    # <bound method Tag.get_text of <trkpt lat="37.8150756843" lon="-122.2536893003"><ele>27.15</ele><time>2020-07-11T18:49:59Z</time><extensions><gpxtpx:TrackPointExtension><gpxtpx:atemp>23.6</gpxtpx:atemp></gpxtpx:TrackPointExtension></extensions></trkpt>>
-    # <trkpt lat="37.8150304221" lon="-122.2539115045"><ele>28.96</ele><time>2020-07-11T23:39:41Z</time></trkpt>
-    trackPoint = []
     latitude = Decimal(record[42:55])
     longitude = Decimal(record[62:77])
     elevation = Decimal(record[84:89])
@@ -66,16 +64,7 @@ def parseGPXLine(record):
     x = myTrackPoint(latitude, longitude, elevation,utc,createLocalTimeObject(utc), temp)
     return x
     
-    # temp1 = Decimal(9.0/5.0)
-
-    # temperature = (temp * temp1) + 32
-    # * (9/5)) + 32.0 
-    # temp = record[84:88]
-# print today.strftime('We are the %d, %b %Y')
-    # print('Lat: ' + str(latitude) + ' | Longitude = ' + str(longitude) + ' | Elevation = ' + str(elevation) + ' | Date = ' + utcDate + ' | Time = ' + utcTime + ' | Temperature = ' + str(temp))
     print('Lat: ' + str(latitude) + ' | Longitude = ' + str(longitude) + ' | Elevation = ' + str(elevation) + ' | Date = ' + utc.strftime ('%d-%b-%Y %H:%M:%S') + ' | ' + createLocalTimeObject(utc).strftime ('%d-%b-%Y %H:%M:%S') + '| Temperature = ' + str(temp))
-
-
 
 
 def readGPX(targetGPX):
@@ -89,6 +78,7 @@ def readGPX(targetGPX):
     for i in range(0, len(trackPoints)):
         # print (str(trackPoints[i].get_text) + ' | ' + str(len(str(trackPoints[i].get_text))))
         distinctTrackPoint = parseGPXLine(str(trackPoints[i].get_text))
+        print('Model: ' + distinctTrackPoint.model)
         print('Latitude: ' + str(distinctTrackPoint.latitude))
         print('Longitude: ' + str(distinctTrackPoint.longtitude))
         print('Elevation: ' + str(distinctTrackPoint.elevation))
@@ -99,25 +89,6 @@ def readGPX(targetGPX):
         else:
             print ('Temperature (f): N/A')
         print('---------------------------------------')
-        # f'{a:.2f}'
-        # print (time[i].get_text)
-        # print (elevation[i].get_text)
-        # print (temp[i].get_text)
-
-
-# # METHOD 2: Auto-detect zones:
-# from_zone = tz.tzutc()
-# to_zone = tz.tzlocal()
-
-# # utc = datetime.utcnow()
-# utc = datetime.strptime('2011-01-21 02:37:21', '%Y-%m-%d %H:%M:%S')
-
-# # Tell the datetime object that it's in UTC time zone since 
-# # datetime objects are 'naive' by default
-# utc = utc.replace(tzinfo=from_zone)
-
-# # Convert time zone
-# central = utc.astimezone(to_zone)
 
 def createLocalTimeObject(utcDate):
     return utcDate.astimezone(tz.gettz())
@@ -131,19 +102,6 @@ def createUTCTimeObject(xTime,xDate):
     second = int(xTime[6:])
 
     return datetime(year, month, day, hour, minute, second, 0, tzinfo=timezone.utc)
-
-    # print(year + ' ' + month + ' ' + day + ' | ' + hour + ':' + minute + ':' + second)
-
-    # infile = open("books.xml","r")
-    # contents = infile.read()
-    # soup = BeautifulSoup(contents,'xml')
-    # titles = soup.find_all('title')
-    # authors = soup.find_all('author')
-    # prices = soup.find_all('price')
-    # for i in range(0, len(titles)):
-    #     print(titles[i].get_text(),"by",end=' ')
-    #     print(authors[i].get_text(),end=' ')
-    #     print("costs $" + prices[i].get_text())
 
     
     # filteredList.append(rawList[3])
